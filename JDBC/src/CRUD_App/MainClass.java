804199a21d35c51bc1e7fbcf18cf61e00c3d7fe1
SHabaj-dev <shabajansari843@gmail.com>
2022-11-29 16:53:04 +0530

    Completed

--- JDBC/src/CRUD_App/MainClass.java
+++ JDBC/src/CRUD_App/MainClass.java
@@ -7,12 +7,11 @@ public class MainClass {
         Scanner sc = new Scanner(System.in);
         StudentDAO dao = new StudentDAO();
         while(true){
-            System.out.println("Please Selct One Option");
+            System.out.println("Please Select One Option");
             System.out.println("1 - Show Whole Table");
             System.out.println("2 - Read Data");
             System.out.println("3 - Insert Data");
-            System.out.println("4- Delete Data");
-            System.out.println("5- Update Data");
+            System.out.println("4 - Delete Data");
             System.out.println("0 - Exit");
             int choice = sc.nextInt();
 
@@ -28,6 +27,36 @@ public class MainClass {
                     int student_id = sc.nextInt();
                     dao.showData(student_id);
                     System.out.println();
+                }
+
+                case 3 -> {
+                    System.out.println("Enter the Student Id, First Name, Last Name And Branch of the Student Sequentially ");
+                    int student_id = sc.nextInt();
+                    String first_name = sc.next();
+                    String last_name = sc.next();
+                    String branch = sc.next();
+                    int rowsAffected = dao.insert(student_id, first_name, last_name, branch);
+                    if(rowsAffected > 0){
+                        System.out.println("Data Insertion Completed");
+                        System.out.println("Total number of Rows Affected " + rowsAffected);
+                    }else{
+                        System.out.println("Inserting Data Failed !! Try Again");
+                    }
+
+                }
+
+                case 4 -> {
+                    System.out.println("Enter Student Id ");
+                    int student_id = sc.nextInt();
+                    int rowsAffected = dao.deleteData(student_id);
+                    if(rowsAffected > 0){
+                        System.out.println("Deletion Successful");
+                    }else{
+                        System.out.println("Something Went Wrong!! Please Try Again");
+                    }
+                }
+                case 0 -> {
+                    System.exit(0);
                 }
             }
         }
--- JDBC/src/CRUD_App/StudentDAO.java
+++ JDBC/src/CRUD_App/StudentDAO.java
@@ -1,5 +1,6 @@
 package CRUD_App;
 
+import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 
 public class StudentDAO {
@@ -9,6 +10,15 @@ public class StudentDAO {
 
     Connection con;
 
+    {
+        try {
+            con = DriverManager.getConnection(url, userName, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    Statement st;
 
     void print(ResultSet rs) throws SQLException {
         //                System.out.println(rs.getInt(1) + " \t " + rs.getString(2) + " \t " + rs.getString(3)
@@ -22,13 +32,15 @@ public class StudentDAO {
         final String query = "SELECT * FROM student";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(url, userName, password);
+//            con = DriverManager.getConnection(url, userName, password);
             Statement st = con.createStatement();
             ResultSet rs = st.executeQuery(query);
 
             while(rs.next()){
                 print(rs);
             }
+            rs.close();
+            con.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -38,7 +50,7 @@ public class StudentDAO {
     public void showData(int student_id){
         final String query = "SELECT * FROM student WHERE student_id = ?";
         try {
-            Statement st = con.createStatement();
+            st = con.createStatement();
             PreparedStatement ps = con.prepareStatement(query);
             ps.setInt(1, student_id);
             ResultSet rs = ps.executeQuery();
@@ -46,10 +58,60 @@ public class StudentDAO {
             while(rs.next()){
                 print(rs);
             }
+            ps.close();
+            con.close();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public int insert(int student_id, String first_name, String last_name, String branch){
+        Student s = new Student();
+        s.setStudent_id(student_id);
+        s.setFirst_name(first_name);
+        s.setLast_name(last_name);
+        s.setBranch(branch);
+        final String query = "INSERT INTO student VALUE (?, ?, ?, ?)";
+        try {
+
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, s.getStudent_id());
+            ps.setString(2, s.getFirst_name());
+            ps.setString(3, s.getLast_name());
+            ps.setString(4, s.getBranch());
+            int rowsAffected = ps.executeUpdate();
+            ps.close();
+            con.close();
+            return rowsAffected;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+
+    }
+
+    public int deleteData(int student_id){
+        final String query = "DELETE FROM student WHERE student_id = ?";
+        int rowsAffected = 0;
+        try {
+            Student s = new Student();
+            s.setStudent_id(student_id);
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, s.getStudent_id());
+            rowsAffected = ps.executeUpdate();
+            ps.close();
+            con.close();
+            return rowsAffected;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return rowsAffected;
+    }
+
 
 }
